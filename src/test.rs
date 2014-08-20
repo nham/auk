ecf23004dafec39d0cb300e2eed5249920b64ad0
nham <hamann.nick@gmail.com>
2014-08-20 19:05:01 -0400

    Test the dummy macro

--- src/test.rs
+++ src/test.rs
@@ -1,3 +1,10 @@
-fn main() {
+#![feature(phase)]
 
+#[phase(plugin)] extern crate auk;
+extern crate auk;
+
+fn main() {
+    let g: auk::PEGGrammar<char, String> = auk!(grammar foo {});
+
+    println!("{}", g.parse(&auk::Empty, &['a', 'b', 'c']));
 }
